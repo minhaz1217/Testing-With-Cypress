dfc37eaf351a7371f5fb968f028693a67c647b39
Minhaz <minhaz1217@gmail.com>
2023-01-22 22:07:01 +0600

    file upload test working

--- cypress/e2e/FireUpload.spec.cy.js
+++ cypress/e2e/FireUpload.spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://cgi-lib.berkeley.edu/ex/fup.html')
-    cy.get('[name=upfile]').attachFile('file/test.txt');
+    cy.get('[name=upfile]').selectFile('cypress/fixtures/file/file_for_upload_test.txt');
     cy.get("[value=Press]").click();
   })
 })
