7f08cd3b05c76fc9d20cf72764324de8aeab160c
Minhaz <minhaz1217@gmail.com>
2023-01-28 00:43:03 +0600

    08. api testing done

--- cypress/e2e/08. API Testing/ApiTest.spec.cy.js
+++ cypress/e2e/08. API Testing/ApiTest.spec.cy.js
@@ -1,28 +1,41 @@
 /// <reference types="cypress" />
 
 describe("API Test Automation", () => {
-  before(() => {
-    cy.visit("https://reqres.in/");
-  });
+  //   before(() => {
+  //     // cy.visit("https://reqres.in/");
+  //   });
 
   it("Get Test", () => {
-    cy.request("GET", "/api/users?page=2").then(() => {
+    cy.request("GET", "https://reqres.in/api/users?page=2").then(() => {
       (response) => {
         expect(response.status).equal(200);
         expect(response.body.data[1].id).equal(8);
         expect(response.body).to.have.property("total_pages");
       };
     });
+  });
+  it("Post Test", () => {
+    cy.request("POST", "https://reqres.in/api/users", {
+      name: "Minhaz",
+      job: "consultant",
+    }).then((response) => {
+      expect(response.status).equal(201);
+      expect(response.body).to.have.property("name", "Minhaz");
+    });
+  });
 
-    it("Post Test", ()=>{
-        cy.request("POST", "/api/users", {
-            "name" : "Minhaz",
-            "job" : "consultant"
-        }).then( (response)=>{
-            expect(response.status).equal(201);;
-            expect(response.body).to.have.property("name", "Minhaz");
-        } );
-    })
+  it("Put Test", () => {
+    cy.request("PUT", "https://reqres.in/api/users/2", {
+      name: "Khan",
+      job: "engineer",
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+  });
 
+  it("Delete Test", () => {
+    cy.request("DELETE", "https://reqres.in/api/users/2").then((response) => {
+      expect(response.status).equal(204);
+    });
   });
 });
